0e376cac33fb4d559b7c26698462319be609a40c
Karen Olson <karmarie@gmail.com>
2021-12-16 12:44:32 -0600

    Refactor show method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,36 @@
 class UsersController < ApplicationController
+before_action :authorize, only: [:show]
 wrap_parameters format: []
 
-    def index 
-        render json: User.all
-    end
-
-    def show
-        user = User.find_by(id: session[:user_id])
-        if user 
-            render json: user 
-        else 
-            render json: {error: "Not authorized"}, status: :unauthorized
-        end
-    end
-
-    def create 
+    def create
         user = User.new(user_params)
         if user.save
             session[:user_id] = user.id
             render json: user, status: :created
         else
-            render json: {errors: user.errors.full_messages}, status: :unprocessable_entity
+            render json: { error: user.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
+    # def show
+    #     user = User.find_by(id: session[:user_id])
+    #     if user 
+    #         render json: user 
+    #     else 
+    #         render json: {error: "Not authorized"}, status: :unauthorized
+    #     end
+    # end
+        
+    def show
+        user = User.find_by(id: session[:user_id])
+        render json: user
+    end
+
     private
+
+    def authorize
+        return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+    end
 
     def user_params
         params.permit(:username, :password, :password_confirmation)
